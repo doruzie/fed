40cb150c5de2a7419e4b0751d640fb70a9e942c9
Dorus Bos <dorusbos@gmail.com>
2019-10-29 21:31:23 +0100

    Smooth scroll

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,11 +1,22 @@
 const $ = jQuery;
 $(document).ready(function(){
+
+    // Page animation script - Dorus Bos
     $("nav a").click(function (e) {
         e.preventDefault();
         var href = $(this).attr('href');
         $("main").addClass("close");
         setTimeout(function () {
             window.location.href = href;
+        }, 500);
+    });
+
+    // Smooth anchor scrolling - samuel Kyama from https://css-tricks.com/snippets/jquery/smooth-scrolling/
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
         }, 500);
     });
 }());
